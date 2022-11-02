4a3aadbbea477b3b179ec4cbd5352a5434bb9de7
natali <devidvarl@gmail.com>
2022-11-02 08:10:36 +0100

    format selection

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let arr = ["Orange", "Grapefruit", "Pear"];
 let sorted = copySorted(arr);
 function copySorted(arr) {
     return arr.slice().sort();
-  }
-  alert(sorted);
-  alert(arr);
+}
+alert(sorted);
+alert(arr);
